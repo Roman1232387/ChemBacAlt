45c323135298589139fd364dec4d097c5802ea5f
danusorocan1 <daniel.sorocan@isa.utm.md>
2026-03-25 15:14:26 +0200

    autentificarea

--- src/mock/users.ts
+++ src/mock/users.ts
@@ -19,4 +19,22 @@ export const mockUsers: User[] = [
     avatarInitials: 'AP',
     createdAt: '2024-02-15T09:00:00Z',
   },
+  {
+    id: 'u3',
+    email: 'maria.ion@liceu.ro',
+    password: 'Maria2025!',
+    name: 'Maria Ion',
+    role: 'user',
+    avatarInitials: 'MI',
+    createdAt: '2024-03-10T10:00:00Z',
+  },
+  {
+    id: 'u4',
+    email: 'alexandru.dumitrescu@scoala.ro',
+    password: 'Alex2025!',
+    name: 'Alexandru Dumitrescu',
+    role: 'user',
+    avatarInitials: 'AD',
+    createdAt: '2024-03-15T11:00:00Z',
+  },
 ];
