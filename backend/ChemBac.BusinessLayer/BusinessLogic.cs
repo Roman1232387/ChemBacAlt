221d3920946cadcd681458b85f34577814873efb
Roman1232387 <rimbu14102005@gmail.com>
2026-04-22 02:59:58 +0300

    feat: adaugat BusinessLogic si clase skeleton

---
+++ backend/ChemBac.BusinessLayer/BusinessLogic.cs
@@ -0,0 +1,24 @@
+using ChemBac.BusinessLayer.Interfaces;
+using ChemBac.BusinessLayer.Structure;
+
+namespace ChemBac.BusinessLayer;
+
+public class BusinessLogic
+{
+    public BusinessLogic() { }
+
+    public ILessonAction LessonAction()
+    {
+        return new LessonExecution();
+    }
+
+    public ITestAction TestAction()
+    {
+        return new TestExecution();
+    }
+
+    public IUserAction UserAction()
+    {
+        return new UserExecution();
+    }
+}
